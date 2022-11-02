d312b2f28b14e03e2ef89d54fe786221be58eab4
swordlight666 <swordlight666@163.com>
2022-11-02 23:26:07 +0800

    ver 1.0

---
+++ src/main/java/burp/BurpExtender.java
@@ -0,0 +1,279 @@
+package burp;
+
+import jdk.nashorn.internal.ir.debug.JSONWriter;
+
+import java.awt.Component;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+
+public class BurpExtender extends AbstractTableModel implements IBurpExtender, ITab, IHttpListener, IMessageEditorController
+{
+    private IBurpExtenderCallbacks callbacks;
+    private IExtensionHelpers helpers;
+    private JSplitPane splitPane;
+    private IMessageEditor requestViewer;
+    private IMessageEditor responseViewer;
+    private final List<LogEntry> log = new ArrayList<LogEntry>();
+    private IHttpRequestResponse currentlyDisplayedItem;
+    private PrintWriter stdout;
+    private PrintWriter stderr;
+
+
+    //
+    // implement IBurpExtender
+    //
+    
+    @Override
+    public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks)
+    {
+        // keep a reference to our callbacks object
+        this.callbacks = callbacks;
+
+        stdout = new PrintWriter(callbacks.getStdout(), true);
+        stderr = new PrintWriter(callbacks.getStderr(), true);
+        // obtain an extension helpers object
+        helpers = callbacks.getHelpers();
+        stdout.println(System.getProperty("user.dir"));
+
+//        this.callbacks.restoreState(new File("xss.file"));
+
+        // set our extension name
+        callbacks.setExtensionName("Json Xss Finder");
+
+        
+        // create our UI
+        SwingUtilities.invokeLater(new Runnable() 
+        {
+            @Override
+            public void run()
+            {
+                // main split pane
+                splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+                        
+                // table of log entries
+                Table logTable = new Table(BurpExtender.this);
+                JScrollPane scrollPane = new JScrollPane(logTable);// add scroll
+                splitPane.setLeftComponent(scrollPane);
+
+                // tabs with request/response viewers
+                JTabbedPane tabs = new JTabbedPane();
+                requestViewer = callbacks.createMessageEditor(BurpExtender.this, false);
+                responseViewer = callbacks.createMessageEditor(BurpExtender.this, false);
+                tabs.addTab("Request", requestViewer.getComponent());
+                tabs.addTab("Response", responseViewer.getComponent());
+                splitPane.setRightComponent(tabs);
+
+                // customize our UI components
+                callbacks.customizeUiComponent(splitPane);
+                callbacks.customizeUiComponent(logTable);
+                callbacks.customizeUiComponent(scrollPane);
+                callbacks.customizeUiComponent(tabs);
+                
+                // add the custom tab to Burp's UI
+                callbacks.addSuiteTab(BurpExtender.this);
+                
+                // register ourselves as an HTTP listener
+                callbacks.registerHttpListener(BurpExtender.this);
+            }
+        });
+    }
+
+
+
+
+
+
+    //
+    // implement ITab
+    //
+
+    @Override
+    public String getTabCaption()
+    {
+        return "Json Xss Finder";
+    }
+
+    @Override
+    public Component getUiComponent()
+    {
+        return splitPane;
+    }
+
+    //
+    // implement IHttpListener
+    //
+    
+    @Override
+    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo)
+    {
+        // only process responses
+        if (!messageIsRequest)
+        {
+            // create a new log entry with the message details
+            synchronized(log)
+            {
+                IResponseInfo responseInfo = helpers.analyzeResponse(messageInfo.getResponse());
+                IRequestInfo requestInfo = helpers.analyzeRequest(messageInfo.getRequest());
+
+                String statedMimeType = responseInfo.getStatedMimeType();
+                String inferredMimeType = responseInfo.getInferredMimeType();
+                if(statedMimeType.equals("HTML")&&inferredMimeType.equals("JSON")){
+                    int row = log.size();
+                    log.add(new LogEntry(requestInfo.getMethod(), callbacks.saveBuffersToTempFiles(messageInfo),
+                            helpers.analyzeRequest(messageInfo).getUrl()));
+                    fireTableRowsInserted(row, row);
+
+
+
+                }
+
+            }
+        }
+    }
+
+    //
+    // extend AbstractTableModel
+    //
+    
+    @Override
+    public int getRowCount()
+    {
+        return log.size();
+    }
+
+    @Override
+    public int getColumnCount()
+    {
+        return 3;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex)
+    {
+        switch (columnIndex)
+        {
+            case 0:
+                return "ID";
+            case 1:
+                return "METHOD";
+            case 2:
+                return "URL";
+            default:
+                return "";
+        }
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex)
+    {
+        return String.class;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex)
+    {
+        LogEntry logEntry = log.get(rowIndex);
+
+        switch (columnIndex)
+        {
+            case 0:
+                return rowIndex;
+            case 1:
+                return logEntry.method;
+            case 2:
+                return logEntry.url.toString();
+            default:
+                return "";
+        }
+    }
+
+    //
+    // implement IMessageEditorController
+    // this allows our request/response viewers to obtain details about the messages being displayed
+    //
+    
+    @Override
+    public byte[] getRequest()
+    {
+        return currentlyDisplayedItem.getRequest();
+    }
+
+    @Override
+    public byte[] getResponse()
+    {
+        return currentlyDisplayedItem.getResponse();
+    }
+
+    @Override
+    public IHttpService getHttpService()
+    {
+        return currentlyDisplayedItem.getHttpService();
+    }
+
+    //
+    // extend JTable to handle cell selection
+    //
+
+
+
+    
+    private class Table extends JTable
+    {
+        public Table(TableModel tableModel)
+        {
+            super(tableModel);
+        }
+        
+        @Override
+        public void changeSelection(int row, int col, boolean toggle, boolean extend)
+        {
+            // show the log entry for the selected row
+            LogEntry logEntry = log.get(row);
+            requestViewer.setMessage(logEntry.requestResponse.getRequest(), true);
+            responseViewer.setMessage(logEntry.requestResponse.getResponse(), false);
+            currentlyDisplayedItem = logEntry.requestResponse;
+            
+            super.changeSelection(row, col, toggle, extend);
+        }
+
+
+    }
+    
+    //
+    // class to hold details of each log entry
+    //
+    
+    private static class LogEntry
+    {
+        final IHttpRequestResponsePersisted requestResponse;
+        final URL url;
+        final String method;
+
+        LogEntry(String method, IHttpRequestResponsePersisted requestResponse, URL url)
+        {
+            this.method = method;
+            this.requestResponse = requestResponse;
+            this.url = url;
+        }
+
+        @Override
+        public String toString() {
+            return "LogEntry{" +
+                    "requestResponse=" + requestResponse +
+                    ", url=" + url +
+                    ", method='" + method + '\'' +
+                    '}';
+        }
+    }
+}
